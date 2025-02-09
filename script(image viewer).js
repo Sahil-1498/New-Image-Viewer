693430e31e6cfdb2eb3fde1c23364021d18492f0
Sahil-1498 <sahil123tp@gmail.com>
2025-02-09 21:17:38 +0530

    Add files via upload

---
+++ script(image viewer).js
@@ -0,0 +1,370 @@
+let btn1 = document.getElementById("btn1");
+
+
+let btn30 = document.getElementById("btn30");
+
+
+let imgshow = document.getElementById("imgshow");
+let upper1 = document.getElementById("upper")
+let lower1 = document.getElementById("lower")
+btn1.onclick = function show()
+{   
+    imgshow.src = "1.png";
+    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+// .............................
+btn2.onclick = function show()
+{   
+    imgshow.src = "2.png";
+    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+// .....................................
+btn3.onclick = function show()
+{   
+    imgshow.src = "3.png";
+    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn4.onclick = function show()
+{   
+    imgshow.src = "4.png";
+    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn5.onclick = function show()
+{   
+    imgshow.src = "5.png";
+    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn6.onclick = function show()
+{   
+    imgshow.src = "6.png";
+    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn7.onclick = function show()
+{   
+    imgshow.src = "7.png";
+    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn8.onclick = function show()
+{   
+    imgshow.src = "8.png";
+    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn9.onclick = function show()
+{   
+    imgshow.src = "9.png";
+    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn10.onclick = function show()
+{   
+    imgshow.src = "10.png";
+    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+    
+}
+btn11.onclick = function show()
+{   
+    imgshow.src = "11.png";
+    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn12.onclick = function show()
+{   
+    imgshow.src = "1.png";
+    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn13.onclick = function show()
+{   
+    imgshow.src = "2.png";
+    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn14.onclick = function show()
+{   
+    imgshow.src = "3.png";
+    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+
+btn15.onclick = function show()
+{   
+    imgshow.src = "4.png";
+    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn16.onclick = function show()
+{   
+    imgshow.src = "5.png";    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn17.onclick = function show()
+{   
+    imgshow.src = "6.png";    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn18.onclick = function show()
+{   
+    imgshow.src = "7.png";    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn19.onclick = function show()
+{   
+    imgshow.src = "8.png";    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn20.onclick = function show()
+{   
+    imgshow.src = "9.png";    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn21.onclick = function show()
+{   
+    imgshow.src = "10.png";    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn22.onclick = function show()
+{   
+    imgshow.src = "11.png";    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn23.onclick = function show()
+{   
+    imgshow.src = "1.png";    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn24.onclick = function show()
+{   
+    imgshow.src = "2.png";    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn25.onclick = function show()
+{   
+    imgshow.src = "3.png";    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn26.onclick = function show()
+{   
+    imgshow.src = "4.png";    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn27.onclick = function show()
+{   
+    imgshow.src = "5.png";    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn28.onclick = function show()
+{   
+    imgshow.src = "6.png";    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+btn29.onclick = function show()
+{   
+    imgshow.src = "7.png";    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+}
+
+btn30.onclick = function show()
+{
+    imgshow.src = "8.png";    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+
+    upper1.onclick = function()
+    {
+        lower1.style.display = "flex";
+        upper1.style.width = "80vw";
+    }
+    lower1.style.display = "none";
+    upper1.style.width = 100+"vw";
+}
+
+
+// ...................................
+
+lower1.onblur = function lowerhide()
+{
+    lower1.style.display = "none";
+    upper1.style.width = "100vw";
+}
